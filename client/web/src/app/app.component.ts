c0a70fb6fef6b4a67ae234c45f44f6f6f960ae26
Igor Amidzic <amidzicigor46@gmail.com>
2019-07-18 12:15:09 -0400

    add more detailed theming options

--- client/web/src/app/app.component.ts
+++ client/web/src/app/app.component.ts
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  themes: string[] = [
+    'theme-open-up',
+    'theme-open-down',
+    'theme-closed-up',
+    'theme-closed-down'
+  ];
+  // theme: string = 'theme-open-up';
+
+  // openUpTheme(): void {
+  //   this.theme = 'theme-open-up';
+  // }
+
+  // openDownTheme(): void {
+  //   this.theme = 'theme-open-down';
+  // }
+
+  // closedUpTheme(): void {
+  //   this.theme = 'theme-closed-up';
+  // }
+
+  // closedDownTheme(): void {
+  //   this.theme = 'theme-closed-down';
+  // }
 }
